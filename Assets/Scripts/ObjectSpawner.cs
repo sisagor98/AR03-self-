171fffbd9f3eff557e80c642b6703e6f5e336623
shariful islam <sisagor98@gmail.com>
2020-05-04 22:11:31 +0600

    commit

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+
+    public GameObject Objectspawn;
+    private Detectplane detectplace;
+
+    private void Start()
+    {
+        detectplace = FindObjectOfType<Detectplane>();
+    }
+
+    private void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(Objectspawn, detectplace.transform.position, detectplace.transform.rotation);
+        }
+    }
+
+}
